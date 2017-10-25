9d09fa3901feaa87266c24a5d1078b5874f1855d
Ni Chia <nichia@gmail.com>
2017-10-25 15:00:33 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,7 +3,7 @@
 
 def key_for_min_value(name_hash)
   lowest_key = nil
-  compared_value = Integer::MAX
+  compared_value = Floate::MAX
   if name_hash != {}
     name_hash.each do |hash_key, hash_value|
       if hash_value < compared_value
