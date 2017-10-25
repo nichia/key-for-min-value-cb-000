87d2d98b5dbcdee591000678a53c72d86d0cbdc5
Ni Chia <nichia@gmail.com>
2017-10-25 14:57:01 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -8,6 +8,7 @@ def key_for_min_value(name_hash)
     name_hash.each do |hash_key, hash_value|
       if hash_value < compared_value
         lowest_key = hash_key
+      end
     end
   end
   lowest_key
